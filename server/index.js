4a7f92dbc64b26c8810626b91b0cab0c146515c7
Ivan <thanhkahp2002@gmail.com>
2023-12-07 22:58:39 +0300

    start func

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const sequelize = require('./db');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// Все операции с БД являются асинхронными
+const start = async () => {
+    try {
+        await sequelize.authenticate(); // Установление подключения к БД
+        await sequelize.sync(); // Сверяет состояние БД со схемой
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
+
